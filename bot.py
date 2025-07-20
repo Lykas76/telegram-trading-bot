d17b27b073ff9c7ebb34c9cc32fdf88ee0bc3e37
Lykas76 <lykas7694@gmail.com>
2025-07-20 08:08:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import aiohttp
